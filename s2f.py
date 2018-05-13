e7aeb26a2551a1094c79e0551a2bd5e0a337b933
Miguel Afonso <mafonso@gmail.com>
2018-05-13 19:04:12 +0100

    First stab at a converter tool.
    Handles conversion for basic FreeAgent format

---
+++ s2f.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+
+
+def convertCSV(filename):
+    with open(filename, "rt") as source, open("fa-" + filename, "wt") as result:
+        rdr = csv.reader(source)
+        wtr = csv.writer(result, delimiter=',', )
+        next(rdr)  # Skip CSV headers
+        next(rdr)  # Skip "Opening balance."
+        for row in rdr:
+            wtr.writerow([row[0], row[4], row[2]])
+
+
+def main(argv):
+    convertCSV(argv[0])
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
